e7b5771cdd72dc9aefbd05b5a6ebf0fb63955b91
HarmanBoparai <erharmanboparai@gmail.com>
2022-11-09 09:41:46 -0500

    testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog, User } = require('../models');
+const { Blog, User } = require('../Moxdels');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
